3da09b63eeaba40816923fa39a8b3a1bf3e2a9ab
Franciszek Nurkiewicz <318212@via.dk>
2022-11-09 23:40:54 +0100

    httpclient auth interfacee and jwtservices

---
+++ Domain/DTOs/UserBasicDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.DTOs;
+
+public class UserBasicDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public UserBasicDto(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
---
+++ HttpClients/ClientInterfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IAuthService
+{
+    public Task LoginAsync(UserLoginDto dto);
+    public Task LogoutAsync();
+    public Task RegisterAsync(User user);
+    public Task<ClaimsPrincipal> GetAuthAsync();
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+}
